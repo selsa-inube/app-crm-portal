c18182ee5a8f270ebf611c7634d056b4ca173e57
LinconOssa <lossa@sistemasenlinea.com.co>
2025-06-06 08:16:44 -0500

    fix: rename

--- src/pages/prospect/components/modals/IncomeModal/config.ts
+++ src/pages/prospect/components/modals/IncomeModal/config.ts
@@ -1,7 +1,7 @@
 export const dataIncomeModal = {
   flagTittle: "¡Información actualizada!",
   flagDescription: "Las fuentes de ingreso fueron actualizadas.",
-  title: "Fuentes de ingreaaaso",
+  title: "Fuentes de ingreso",
   close: "Cerrar",
   save: "Guardar",
   cancel: "Cancelar",
