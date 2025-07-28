2ca95b9f53af9160a8fa0477554eebdef4e9a518
Miller Ostos <millerandresoc@gmail.com>
2025-07-28 17:28:33 -0500

    fix: include the chagens sugecten on the PR

--- src/pages/simulateCredit/steps/ProductSelection/config.ts
+++ src/pages/simulateCredit/steps/ProductSelection/config.ts
@@ -18,8 +18,6 @@ export const electionData = {
   questions: {
     includeExtraordinaryInstallments:
       "¿Quiere programar abonos especiales a capital para disminuir su pago ordinario o recortar plazo?",
-    /* updateIncomeSources:
-      "¿Desea actualizar la información del cliente sobre sus fuentes de ingreso para respaldar su capacidad de pago? Actualmente, estas suman un total de $valor.", */
     updateFinancialObligations:
       "¿Quiere actualizar la información relacionada con las obligaciones financieras del cliente?",
     includeAditionalBorrowers: "¿Quiere registrar deudores adicionales?",
