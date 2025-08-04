2c996b71228279bf831b7d73f5dfb998194f04a9
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2025-08-04 14:29:12 -0500

    feat: create cards in applications credit

---
+++ src/mocks/creditApplication/creditApplication.mock.ts
@@ -0,0 +1,23 @@
+export const mockCreditApplication = [
+  {
+    prospectId: "68531b2426ac1368108249f5",
+    prospectCode: "SC-000000318",
+    timeOfCreation: "2025-06-18T20:01:40.81Z",
+    selectedRegularPaymentSchedule:
+      "Aceptación por parte del cliente de la propuesta.",
+    preferredPaymentChannelAbbreviatedName: "Gestión comercial",
+    moneyDestinationAbbreviatedName:
+      "Educación de postgrado a menos de tres meses.",
+    value: 10000000,
+  },
+  {
+    prospectId: "68531c8726ac1368108249f8",
+    prospectCode: "SC-000000324",
+    timeOfCreation: "2025-06-18T20:07:35.22Z",
+    selectedRegularPaymentSchedule:
+      "Aceptación por parte del cliente de la propuesta.",
+    preferredPaymentChannelAbbreviatedName: "Otro estado",
+    moneyDestinationAbbreviatedName: "Vacaciones",
+    value: 3000000,
+  },
+];
