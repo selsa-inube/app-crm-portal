9e6076af0211abf02f569e8ac8fa51a59f519c8b
carin <ca.rincon97@gmail.com>
2025-09-09 18:10:13 -0500

    fix: adjusting envs

--- src/utils/encrypt/encrypt.ts
+++ src/utils/encrypt/encrypt.ts
@@ -1,10 +1,7 @@
 import CryptoJS from "crypto-js";
-// import { secretKeyPortalId } from "@config/environment";
+import { secretKeyPortalId } from "@config/environment";
 
-// const secretKey = CryptoJS.enc.Hex.parse(secretKeyPortalId);
-const secretKey = CryptoJS.enc.Hex.parse(
-  "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
-);
+const secretKey = CryptoJS.enc.Hex.parse(secretKeyPortalId);
 const iv = CryptoJS.enc.Hex.parse("abcdef9876543210abcdef9876543210");
 const encrypt = (data: string) => {
   return CryptoJS.AES.encrypt(data, secretKey, { iv }).toString();
