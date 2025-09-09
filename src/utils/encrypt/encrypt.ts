f765e0df73a8fabf3b203b39910bc2d4030fa96e
carin <ca.rincon97@gmail.com>
2025-09-09 18:04:34 -0500

    fix: test commit

--- src/utils/encrypt/encrypt.ts
+++ src/utils/encrypt/encrypt.ts
@@ -1,7 +1,10 @@
 import CryptoJS from "crypto-js";
-import { secretKeyPortalId } from "@config/environment";
+// import { secretKeyPortalId } from "@config/environment";
 
-const secretKey = CryptoJS.enc.Hex.parse(secretKeyPortalId);
+// const secretKey = CryptoJS.enc.Hex.parse(secretKeyPortalId);
+const secretKey = CryptoJS.enc.Hex.parse(
+  "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef",
+);
 const iv = CryptoJS.enc.Hex.parse("abcdef9876543210abcdef9876543210");
 const encrypt = (data: string) => {
   return CryptoJS.AES.encrypt(data, secretKey, { iv }).toString();
