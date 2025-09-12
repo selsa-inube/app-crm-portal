8f2acf535a264e599f3b0feec8a679ca9f1d2fed
Miller Ostos <millerandresoc@gmail.com>
2025-09-12 10:11:56 -0500

    refactor: change label

--- src/pages/simulateCredit/steps/loanAmount/config.ts
+++ src/pages/simulateCredit/steps/loanAmount/config.ts
@@ -1,7 +1,7 @@
 export const dataAmount = {
   availableQuota: "Cupo disponible sin garantía.",
-  expectToReceive: "Monto solicitado",
-  amountRequested: "Monto solicitado",
+  expectToReceive: "Valor que el cliente espera recibir.",
+  amountRequested: "Valor que el cliente espera recibir.",
   currentObligations: "¿Quiere abonar a otras obligaciones vigentes?",
   ordinaryPayment: "Medio de atención plan de pago ordinario",
   Periodicity: "Periodicidad",
@@ -9,7 +9,7 @@ export const dataAmount = {
   paymentDate: "Fecha de pago",
   selectOption: "Seleccione una opción",
   creditText: "Cupo disponible sin garantía por línea de crédito",
-  placeholderValue: "Valor que espera recibir",
+  placeholderValue: "",
 };
 
 export const dataModalDisableLoanAmount = {
