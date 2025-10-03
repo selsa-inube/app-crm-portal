195772c8ec7cc97c300c51af6a7e87c62eb2e835
LinconOssa <lossa@sistemasenlinea.com.co>
2025-10-03 17:30:30 -0500

    fix: iauth react

--- src/hooks/useAuthHandler/index.ts
+++ src/hooks/useAuthHandler/index.ts
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useIAuth } from "@inube/iauth-react";
+import { useSignOut } from "../useSignOut";
 
 interface IAuthConfig {
   clientId: string;
@@ -11,8 +12,12 @@ const useAuthHandler = (
   hasAuthError: boolean,
   portalCode: string,
 ) => {
-  const { loginWithRedirect, isAuthenticated, isLoading } = useIAuth();
+  const { loginWithRedirect, isAuthenticated, isLoading, error } = useIAuth();
+  const { signOut } = useSignOut();
 
+  if (error) {
+    signOut("/error?code=1009");
+  }
   useEffect(() => {
     if (
       !hasAuthError &&
