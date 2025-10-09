9920e23c59a6d68478ebfba4039001af6cef757a
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2025-10-09 12:57:27 -0500

    fix: adjust story

--- src/components/modals/CreditLimit/stories/props.ts
+++ src/components/modals/CreditLimit/stories/props.ts
@@ -6,29 +6,5 @@ export const props: Partial<ArgTypes<ICreditLimitProps>> = {
     control: { type: "text" },
     description: "The title of the modal",
   },
-  maxPaymentCapacity: {
-    control: { type: "number" },
-    description: "Maximum payment capacity",
-  },
-  maxReciprocity: {
-    control: { type: "number" },
-    description: "Maximum reciprocity amount",
-  },
-  maxDebtFRC: {
-    control: { type: "number" },
-    description: "Maximum debt under FRC",
-  },
-  assignedLimit: {
-    control: { type: "number" },
-    description: "Assigned credit limit",
-  },
-  maxUsableLimit: {
-    control: { type: "number" },
-    description: "Maximum usable limit",
-  },
-  availableLimitWithoutGuarantee: {
-    control: { type: "number" },
-    description: "Available limit without a guarantee",
-  },
   handleClose: { action: "closed" },
 };
