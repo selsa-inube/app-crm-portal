dc3f793691057696c5384d25cfcb15d9875c3957
miklos12 <millerandresoc@gmail.com>
2025-12-13 19:57:04 -0500

    feat: add a fetch service to check prerequirements

---
+++ src/services/prospect/checkSimulationPrerequisites/index.tsx
@@ -0,0 +1,68 @@
+import {
+  environment,
+  fetchTimeoutServices,
+  maxRetriesServices,
+} from "@config/environment";
+
+import { simulationPrerequisites } from "../types";
+
+export const checkSimulationPrerequisites = async (
+  businessUnitPublicCode: string,
+  clientIdentificationNumber: string,
+): Promise<simulationPrerequisites | null> => {
+  const maxRetries = maxRetriesServices;
+  const fetchTimeout = fetchTimeoutServices;
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), fetchTimeout);
+      const options: RequestInit = {
+        method: "GET",
+        headers: {
+          "X-Action": "CheckSimulationPrerequisites",
+          "X-Business-Unit": businessUnitPublicCode,
+          "Content-type": "application/json; charset=UTF-8",
+          "X-Process-Manager": clientIdentificationNumber,
+        },
+        signal: controller.signal,
+      };
+
+      const res = await fetch(
+        `${environment.VITE_IPROSPECT_QUERY_PROCESS_SERVICE}/prospects/check-simulation-prerequisites/${clientIdentificationNumber}`,
+        options,
+      );
+
+      clearTimeout(timeoutId);
+
+      if (res.status === 204) {
+        return null;
+      }
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw {
+          message: "Ha ocurrido un error: ",
+          status: res.status,
+          data,
+        };
+      }
+
+      return data;
+    } catch (error) {
+      if (attempt === maxRetries) {
+        if (typeof error === "object" && error !== null) {
+          throw {
+            ...(error as object),
+            message: (error as Error).message,
+          };
+        }
+        throw new Error(
+          "Todos los intentos fallaron. No se pudo validar los prerrequisito",
+        );
+      }
+    }
+  }
+
+  return null;
+};
