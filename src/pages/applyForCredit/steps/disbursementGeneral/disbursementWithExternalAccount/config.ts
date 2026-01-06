820107e9405c6e373638ac095b06073b00504649
miklos12 <millerandresoc@gmail.com>
2026-01-06 10:07:33 -0500

    feat: add enums to input select

---
+++ src/pages/applyForCredit/steps/disbursementGeneral/disbursementWithExternalAccount/config.ts
@@ -0,0 +1,11 @@
+import { IOptionsSelect } from "@pages/applyForCredit/types";
+
+export const selectDefaultValue: IOptionsSelect = {
+  id: "0",
+  label: "No hay opciones disponibles",
+  value: "none",
+};
+
+export const errorMessages = {
+  accountType: "Error al cargar los tipos de cuentas",
+};
