d8b03a243c66776595c26324db90105c3fdf8f3a
LinconOssa <lossa@sistemasenlinea.com.co>
2026-01-20 18:16:08 -0500

    fix: integration validate

---
+++ src/services/prospect/validatePrerequisitesForCreditApplication/index.tsx
@@ -0,0 +1,66 @@
+import {
+  environment,
+  fetchTimeoutServices,
+  maxRetriesServices,
+} from "@config/environment";
+import { IValidatePrerequisitesForCreditApplication } from "../types";
+
+export const validatePrerequisitesForCreditApplication = async (
+  businessUnitPublicCode: string,
+  prospectCode: string,
+): Promise<IValidatePrerequisitesForCreditApplication | null> => {
+  const maxRetries = maxRetriesServices;
+  const fetchTimeout = fetchTimeoutServices;
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), fetchTimeout);
+      const options: RequestInit = {
+        method: "GET",
+        headers: {
+          "X-Action": "ValidatePrerequisitesForCreditApplication",
+          "X-Business-Unit": businessUnitPublicCode,
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        signal: controller.signal,
+      };
+
+      const res = await fetch(
+        `${environment.VITE_IPROSPECT_QUERY_PROCESS_SERVICE}/prospects/validate-prerequisites/${prospectCode}`,
+        options,
+      );
+
+      clearTimeout(timeoutId);
+
+      if (res.status === 204) {
+        return null;
+      }
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw {
+          message: "Ha ocurrido un error de validación de prerrequisito: ",
+          status: res.status,
+          data,
+        };
+      }
+
+      return data;
+    } catch (error) {
+      if (attempt === maxRetries) {
+        if (typeof error === "object" && error !== null) {
+          throw {
+            ...(error as object),
+            message: (error as Error).message,
+          };
+        }
+        throw new Error(
+          "Todos los intentos fallaron. No se pudo validar los prerrequisito",
+        );
+      }
+    }
+  }
+
+  return null;
+};
