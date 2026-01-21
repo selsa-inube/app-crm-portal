28967393a05f5af7e1b3f866b905952a765976fb
LinconOssa <lossa@sistemasenlinea.com.co>
2026-01-21 13:22:49 -0500

    fix: minors

--- src/pages/credit/interface.tsx
+++ src/pages/credit/interface.tsx
@@ -28,6 +28,7 @@ import { Fieldset } from "@components/data/Fieldset";
 import { ErrorModal } from "@components/modals/ErrorModal";
 import { InteractiveBox } from "@components/cards/interactiveBox";
 import { BaseModal } from "@components/modals/baseModal";
+import { getUseCaseValue, useValidateUseCase } from "@hooks/useValidateUseCase";
 
 import {
   addConfig,
@@ -71,6 +72,15 @@ const CreditUI = (props: ICreditUIProps) => {
   const isTablet: boolean = useMediaQuery("(max-width: 1024px)");
   const location = useLocation();
   const { addFlag } = useFlag();
+  const { disabledButton: cannotSimulateCredit } = useValidateUseCase({
+    useCase: getUseCaseValue("canSimulateCredit"),
+  });
+
+  const { disabledButton: cannotRequestCredit } = useValidateUseCase({
+    useCase: getUseCaseValue("canRequestCredit"),
+  });
+
+  const isPayrollAdvanceDisabled = cannotSimulateCredit || cannotRequestCredit;
 
   const mergeSubOptions = (
     backendOptions: IOptionStaff[],
@@ -107,6 +117,14 @@ const CreditUI = (props: ICreditUIProps) => {
   );
 
   const handleCardClick = (title: string, url: string, optionId: string) => {
+    // ====== CONTROL DE PRIVILEGIOS ======
+    // Si es la opción de Adelanto de Nómina y NO tiene ambos privilegios
+    if (optionId === "Adelanto de nómina" && isPayrollAdvanceDisabled) {
+      setMessageError(errorDataCredit.noPrivileges.i18n[lang]);
+      setShowErrorModal(true);
+      return;
+    }
+
     if (optionId === advancePaymentModal.tag.subtitle) {
       handleOpenInfoModal();
     } else if (title === advancePaymentModal.textPayrool) {
@@ -152,6 +170,7 @@ const CreditUI = (props: ICreditUIProps) => {
       window.history.replaceState({}, document.title);
     }
   }, [location]);
+
   return (
     <>
       {codeError ? (
@@ -232,7 +251,11 @@ const CreditUI = (props: ICreditUIProps) => {
                             title={title}
                             subtitle={subtitle}
                             url={url}
-                            isDisabled={isDisabled}
+                            isDisabled={
+                              isDisabled ||
+                              (optionId === "Adelanto de nómina" &&
+                                isPayrollAdvanceDisabled)
+                            }
                             onClick={() =>
                               handleCardClick(title, url, optionId)
                             }
