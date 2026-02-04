6ef6501c71fa90eed61c0ad63f19b8413aead362
LinconOssa <lossa@sistemasenlinea.com.co>
2026-02-04 15:15:46 -0500

    fix: text

--- src/pages/applyForCredit/components/GeneralInformationForm/config.ts
+++ src/pages/applyForCredit/components/GeneralInformationForm/config.ts
@@ -4,8 +4,8 @@ export const dataGeneralInformationForm = {
     description:
       "Validation message when identification number matches the user's own",
     i18n: {
-      en: "The entered identification number cannot match your own.",
-      es: "El número de identificación ingresado no puede coincidir con el tuyo",
+      en: "The identification number entered must be different from that of the debtor.",
+      es: "El número de identificación ingresado debe ser diferente al del deudor",
     },
   },
 };
