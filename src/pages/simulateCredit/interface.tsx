be81517cf03bf325b3d3021e544db7305fea68d1
miklos12 <millerandresoc@gmail.com>
2026-02-17 18:25:23 -0500

    fix: refactor genera header

--- src/pages/simulateCredit/interface.tsx
+++ src/pages/simulateCredit/interface.tsx
@@ -30,14 +30,12 @@ import { IProspect } from "@services/prospect/types";
 import { IValidateRequirement } from "@services/requirement/types";
 import { BaseModal } from "@components/modals/baseModal";
 import { IResponsePaymentDatesChannel } from "@services/payment-channels/SearchAllPaymentChannelsByIdentificationNumber/types";
-import userImage from "@assets/images/userImage.jpeg";
 import { EnumType } from "@hooks/useEnum/useEnum";
 import { IAllEnumsResponse } from "@services/enumerators/types";
 import { Fieldset } from "@components/data/Fieldset";
 import { ICreditRiskBureauUpdateMethod } from "@services/creditRiskBureauQueries/types";
 
 import { riskScoreData } from "./steps/riskScore/config";
-import { GeneralHeader } from "./components/GeneralHeader";
 import { ExtraordinaryInstallments } from "./steps/extraordinaryInstallments";
 import { stepsAddProspect } from "./config/addProspect.config";
 import {
@@ -205,7 +203,6 @@ export function SimulateCreditUI(props: SimulateCreditUIProps) {
     navigate,
     currentStepsNumber,
     customerData,
-    dataHeader,
     steps,
     isCurrentFormValid,
     isModalOpenRequirements,
@@ -278,12 +275,6 @@ export function SimulateCreditUI(props: SimulateCreditUIProps) {
             margin="20px 0px"
           >
             <Stack gap="24px" direction="column" height="100%" width="100%">
-              <GeneralHeader
-                buttonText="Agregar vinculación"
-                descriptionStatus={dataHeader.status}
-                name={dataHeader.name}
-                profileImageUrl={dataHeader.image || userImage}
-              />
               <Breadcrumbs
                 crumbs={addConfig.crumbs.map((crumb) => ({
                   ...crumb,
