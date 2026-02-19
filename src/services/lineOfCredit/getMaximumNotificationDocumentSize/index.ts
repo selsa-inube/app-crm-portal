55cbc21d926579763ef4a0fe6fd271f9780c14ba
miklos12 <millerandresoc@gmail.com>
2026-02-19 14:39:37 -0500

    feat: add service to validation

---
+++ src/services/lineOfCredit/getMaximumNotificationDocumentSize/index.ts
@@ -0,0 +1,68 @@
+import {
+  environment,
+  fetchTimeoutServices,
+  maxRetriesServices,
+} from "@config/environment";
+import { IMaximumNotificationDocumentSize } from "../types";
+
+export const getMaximumNotificationDocumentSize = async (
+  businessUnitPublicCode: string,
+  authorizationToken: string,
+): Promise<IMaximumNotificationDocumentSize | null> => {
+  const maxRetries = maxRetriesServices;
+  const fetchTimeout = fetchTimeoutServices;
+  for (let attempt = 1; attempt <= maxRetries; attempt++) {
+    try {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), fetchTimeout);
+      const options: RequestInit = {
+        method: "GET",
+        headers: {
+          "X-Action": "getMaximumNotificationDocumentSize",
+          "X-Business-Unit": businessUnitPublicCode,
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `${authorizationToken}`,
+        },
+        signal: controller.signal,
+      };
+
+      const res = await fetch(
+        `${environment.ICOREBANKING_API_URL_QUERY}/lines-of-credit/`,
+        options,
+      );
+
+      clearTimeout(timeoutId);
+
+      if (res.status === 204) {
+        return null;
+      }
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw {
+          message:
+            "Ha ocurrido un error al obtener el tamaño máximo para el documento de notificación: ",
+          status: res.status,
+          data,
+        };
+      }
+
+      return data;
+    } catch (error) {
+      if (attempt === maxRetries) {
+        if (typeof error === "object" && error !== null) {
+          throw {
+            ...(error as object),
+            message: (error as Error).message,
+          };
+        }
+        throw new Error(
+          "Todos los intentos fallaron. No se pudo obtener el tamaño máximo para el documento de notificación.",
+        );
+      }
+    }
+  }
+
+  return null;
+};
